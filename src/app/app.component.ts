cad2abc64a5d7bd39f35a08c1751bb91075eae1d
AnishSain <37013071+AnishSain@users.noreply.github.com>
2025-03-09 21:32:19 +0530

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { PatientHistoryComponent } from './patient-history/patient-history.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,PatientHistoryComponent],
+  imports: [RouterOutlet,HeaderComponent,PatientHistoryComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { PatientHistoryComponent } from './patient-history/patient-history.component';
 
+
 @NgModule({
-  declarations: [
-    
-  ],
-  imports: [AppComponent,
-    PatientHistoryComponent,
+  declarations: [],
+  imports: [
     BrowserModule,
+    CommonModule,
+    AppRoutingModule,
+    CommonModule,
+    PatientHistoryComponent
   ],
   providers: [],
-  // bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
